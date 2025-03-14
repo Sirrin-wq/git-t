b92fbc211a371edf7dee50df73bb6c45fb1c7325
Yan Bisyukov <yanbisyukov@gmail.com>
2025-03-14 21:54:24 +0300

    add printHello mehtod to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        printHello();
+    }
+
+    public static void printHello(){
         System.out.println("Hello world");
     }
 }
